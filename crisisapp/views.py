06fadd557ab98fb2520b076f0936203f37b80ae4
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2024-07-27 20:20:14 +0300

    ft:templating-structure - modify templates

--- crisisapp/views.py
+++ crisisapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+# from .forms import *
+from django.contrib.auth.decorators import login_required
+
+# APPLICATION VIEWS.
+# home function
+def home(request):
+    return render(request, 'crisis/index.html')
+
